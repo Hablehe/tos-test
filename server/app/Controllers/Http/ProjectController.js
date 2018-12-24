30f24e92681b88d7667b50e6ff21efb51971c486
Remember8217 <1293986298@qq.com>
2018-12-24 15:24:47 +0800

    add update endpoint

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -33,15 +33,15 @@ class ProjectController {
         return project;
     }
 
-    // async update({auth, request, params}) {
-    //     const user = await auth.getUser();
-    //     const {id}=params;
-    //     const project = await Project.find(id);
-    //     AuthorizationService.verifyPermission(project,user);
-    //     project.merge(request.only('title'));
-    //     await project.save();
-    //     return project;
-    // }
+    async update({auth, request, params}) {
+        const user = await auth.getUser();
+        const {id}=params;
+        const project = await Project.find(id);
+        AuthorizationService.verifyPermission(project,user);
+        project.merge(request.only('title'));
+        await project.save();
+        return project;
+    }
 
 }
 
