f6fe699853a231b50f166c96978c5b4391bf844c
Remember8217 <1293986298@qq.com>
2019-01-09 17:11:24 +0800

    create tasks

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -12,6 +12,7 @@
           project,
           title: $event,
         })"
+        @onClick="projectClicked(project)"
         @onEdit="setEditMode(project)"
         @onSave="saveProject(project)"
         @onDelete="deleteProject(project)"
@@ -43,16 +44,24 @@ export default {
     ...mapState('projects', ['newProjectName', 'projects']),
   },
   methods: {
+    projectClicked(project) {
+      this.setCurrentProject(project);
+      this.fetchTasksForProject(project);
+    },
     ...mapMutations('projects', [
       'setNewProjectName',
       'setEditMode',
       'setProjectTitle',
+      'setCurrentProject',
     ]),
     ...mapActions('projects', [
       'createProject',
       'fetchProjects',
       'saveProject',
       'deleteProject',
+    ]),
+    ...mapActions('tasks', [
+      'fetchTasksForProject',
     ]),
   },
 };
